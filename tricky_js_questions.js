a7b806c5086e22c77ff306337b551de38bc3ea2a
Steven Pavlovskii <kirill-pavlovskij@rambler.ru>
2021-04-07 23:49:32 +0300

    added IIFE function question about scopes

---
+++ tricky_js_questions.js
@@ -0,0 +1,7 @@
+// Ответить на вопрос: Что выведеться в консоле?
+(function() {
+    var a = b = 3;
+})();
+
+console.log('a defined ? ' + (typeof a !== "undefined"));
+console.log('b defined ? ' + (typeof b !== "undefined"));
