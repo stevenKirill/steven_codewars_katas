2dd971ddf89b0db45fb8a16faab7d01859fc05df
Кирилл Павловский <kirill.pavlovskiy@gazprombank.ru>
2022-01-15 21:24:16 +0300

    added task

--- 8kyu.js
+++ 8kyu.js
@@ -3,3 +3,23 @@
 function binToDec(bin) {
     return bin.split('').reverse().map((element, i) => element == 1 ? Math.pow(2, i) : 0).reduce((prev, curr) => prev + curr, 0);
 };
+
+function numToSting(num) {
+    const str = String(num); // преобразуешь число в строку
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        if (i % 3 === 0) {
+            result += str[i] + ' '
+        } else {
+            result += str[i]
+        }
+    }
+    return result;
+};
+
+console.log(numToSting(1000000000))
+
+console.log(1 + 1)
+console.log('1' + '1000')
+console.log('hello' + ' world')
+
