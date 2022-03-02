f647bc94eaf012c1b6d5ec5f1d750efdaf437735
Кирилл Павловский <kirill.pavlovskiy@gazprombank.ru>
2022-03-02 14:48:42 +0300

    added some new questins

---
+++ interview_questions.js
@@ -0,0 +1,103 @@
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof undefined);
+
+/////////////////////////////////////
+
+console.log('script  start')
+
+async function async1 () {
+	await async2()
+	console.log('async1 end')
+}
+async function async2 () {
+	console.log('async2 end')
+}
+async1()
+
+setTimeout(() => {
+	console.log('setTimeout')
+})
+
+new Promise(resolve => {
+	console.log('Promise')
+	resolve()
+	.then(function () {
+		console.log('promise1')
+	})
+	.then(function () {
+		console.log('promise2')
+	})
+})
+console.log('script  end')
+
+/////////////////////////////////////
+
+function printing() {
+   console.log(1); 
+   setTimeout(function() { console.log(2); }, 1000); 
+   setTimeout(function() { console.log(3); }, 0); 
+   console.log(4);
+}
+
+printing();
+
+/////////////////////////////////////
+
+function greet(person) {
+  if (person == { name: 'amy' }) {
+    return 'hey amy'
+  } else {
+    return 'hey arnold'
+  }
+}
+greet({ name: 'amy' })
+
+///////////////////////////////////
+for (var i = 0; i < 4; i++) {
+  setTimeout(() => console.log(i), 0)
+}
+
+/////////////////////////////////////
+
+let dog = {
+  name: 'doggo',
+  sayName() {
+    console.log(this.name)
+  },
+}
+let sayName = dog.sayName
+sayName()
+
+////////////////////////////////////
+
+function Dog(name) {
+  this.name = name
+}
+Dog.bark = function () {
+  console.log(this.name + ' says woof')
+}
+let fido = new Dog('fido')
+fido.bark()
+
+////////////////////////////////////
+
+function isBig(thing) {
+    if (thing == 0 || thing == 1 || thing == 2) {
+      return false
+    }
+    return true
+  }
+isBig(1) // false
+isBig([2]) // false
+isBig([3]) // true
+
+////////////////////////////////////
+
+console.log(0 == '0')
+console.log(0 == [])
+console.log('0' == [])
+
+////////////////////////////////////
+
