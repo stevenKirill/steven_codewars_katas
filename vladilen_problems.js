405a3e944e62ed2b52dd5039f82305da81e49bf6
Кирилл Павловский <kirill.pavlovskiy@gazprombank.ru>
2022-04-19 16:13:50 +0300

    added new problems

--- vladilen_problems.js
+++ vladilen_problems.js
@@ -30,15 +30,15 @@ function isUnique2(str) {
     return new Set(str).size === str.length
 };
 
-console.log(isUnique2('abcdef')) // true
-console.log(isUnique2('abcdeafc')) // false
+// console.log(isUnique2('abcdef')) // true
+// console.log(isUnique2('abcdeafc')) // false
 
 // плоский массив
 function flatten(array) {
     return array.reduce((acc,curr) => Array.isArray(curr) ? acc.concat(flatten(curr)) : acc.concat(curr),[])
 };
 
-console.log(flatten([1,[2,3,[4,5,[6,[7,8,[9,[10]]]]]]])) // [1,2,3,4,5,6,7,8,9,10];
+// console.log(flatten([1,[2,3,[4,5,[6,[7,8,[9,[10]]]]]]])) // [1,2,3,4,5,6,7,8,9,10];
 
 // удаление всех повторяющихся значений
 function removeDupes(str) {
@@ -73,3 +73,106 @@ function highestFrequency(array) {
 
 // console.log(highestFrequency(['a','b','c','c','c','d','e']))
 // console.log(highestFrequency(['abc','abc','c','abc','c','def','def','def','ab','ab','def']))
+
+function isStringRotated(source,test) {
+    //return ([...source].every(char => test.includes(char)) && source.length === test.length)
+    if (source.length !== test.length) return false
+
+    for (let i = 0; i < source.length; i++) {
+        const rotate = source.slice(i, source.length) + source.slice(0,i)
+        if (rotate === test) {
+            return true
+        }
+    }
+    return false;
+};
+
+// console.log(isStringRotated('apple','elppa'))
+// console.log(isStringRotated('javascript','scriptjava'))
+// console.log(isStringRotated('javascript','scriptava'))
+
+function arraySubset(source,subset) {
+    if (source.length < subset.length) return false;
+    // for( let i = 0; i < subset.length; i++) {
+    //     if (source.indexOf(subset[i]) === -1) return false;
+    //     delete source[index]
+    // }
+    // return true;
+    const checked = {};
+    for (let i = 0; i < subset.length; i++) {
+        if (checked[subset[i]] && source.includes(subset[i])) return false;
+        if (!source.includes(subset[i])) return false;
+        checked[subset[i]] = true;
+    }
+    return true
+};
+
+// console.log(arraySubset([1,2,3],[2,1,3])) // true
+// console.log(arraySubset([1,1,1,3],[1,3,3])) // false
+// console.log(arraySubset([1,1,1,3,5],[1,3,3,5,5,5])) // false
+
+function allAnagrams(array) {
+    const first = [...array[0]]
+    const sliced = array.slice(1);
+    return first.every((char) => {
+        let allTrue = true;
+        sliced.forEach(word => {
+            if (!word.includes(char)) {
+                allTrue = false;
+            }
+        });
+        return allTrue;
+    });
+};
+
+// console.log(allAnagrams(['abcd','bdac','cabd'])) // true
+// console.log(allAnagrams(['abcd','bdxc','cabd'])) // false
+
+
+const mtrx = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+];
+
+function rotate(matrix) {
+    const rotated = matrix.map(_ => [])
+
+    for (let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
+            rotated[j][matrix.length - 1 -i] = matrix[i][j] 
+        }
+    }
+    return rotated
+}
+
+function rotate180(matrix) {
+    return rotate(rotate(matrix));
+};
+
+function rotate270(matrix) {
+    return rotate180(rotate(matrix))
+};
+
+function printMatrix(arr) {
+    arr.forEach(row => console.log(row))
+};
+
+function rotate360(matrix) {
+    return rotate270(rotate(matrix))
+};
+
+printMatrix(rotate(mtrx))
+console.log('---------------')
+printMatrix(rotate180(mtrx))
+console.log('---------------')
+printMatrix(rotate270(mtrx))
+console.log('---------------')
+printMatrix(rotate360(mtrx))
+// 0:0 0:1 0:2    0:2 1:2 2:2
+// 1:0 1:1 1:2 => 0:1 1:1 2:1
+// 2:0 2:1 2:2    0:0 1:0 2:0
+
+// [1,2,3],    [7,4,1], 
+// [4,5,6], => [8,5,2],
+// [7,8,9],    [9,6,3],
