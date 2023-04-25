b78cfc50d87d57dc85470df645f6ce82e6e18a73
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2023-04-25 10:55:14 +0300

    feat: changed folder structure

--- src/polifills/bind.js
+++ src/polifills/bind.js
@@ -1,11 +1,11 @@
 Function.prototype.bind = function(ctx) {
-    const boundArgs = [].slice.call(arguments, 1);
-    const func = this;
+  const boundArgs = [].slice.call(arguments, 1);
+  const func = this;
 
-    return function() {
-        const callArgs = boundArgs.concat([].slice.call(arguments))
-        return func.apply(ctx, callArgs)
-    }
+  return function() {
+    const callArgs = boundArgs.concat([].slice.call(arguments))
+    return func.apply(ctx, callArgs)
+  }
 }
 
 export default Function.prototype.bind
